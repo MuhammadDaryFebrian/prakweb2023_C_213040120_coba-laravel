c79cba0d2de44e40933ca0d815e17be25ec6d67b
MuhammadDaryFebrian <muhammaddary88@gmail.com>
2023-12-06 13:00:57 +0700

    Menyelesaikan laravel

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,36 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\Sluggable;  
 
 class Post extends Model
 {
     use HasFactory, Sluggable;
 
-    // protected $fillable = ['title', 'excerpt', 'body'];
+    // protected $fillable = ['title','excerpt','body'];
     protected $guarded = ['id'];
-    protected $with = ['category', 'author'];
+    protected $with = ['category','author'];
 
     public function scopeFilter($query, array $filters)
     {
-        $query->when($filters['search'] ?? false, function($query, $search){
+        $query->when($filters['search'] ?? false, function($query, $search) {
             return $query->where('title', 'like', '%' . $search . '%')
-                ->orWhere('body', 'like', '%' . $search . '%');
+                        ->orWhere('body', 'like', '%' . $search . '%');
         });
 
-        $query->when($filters['category'] ?? false, function($query, $category){
-            return $query->whereHas('category', function($query) use ($category){
+        $query->when($filters['category'] ?? false, function($query, $category) {
+            return $query->whereHas('category', function($query) use ($category) {
                 $query->where('slug', $category);
             });
         });
 
         $query->when($filters['author'] ?? false, fn($query, $author) => 
-            $query->whereHas('author', fn($query) => 
+            $query->whereHas('author', fn($query) =>
                 $query->where('username', $author)
             )
         );
     }
-
+    
     public function category()
     {
         return $this->belongsTo(Category::class);
@@ -49,7 +49,7 @@ class Post extends Model
         return 'slug';
     }
 
-    public function sluggable(): array
+    public function Sluggable(): array
     {
         return [
             'slug' => [
